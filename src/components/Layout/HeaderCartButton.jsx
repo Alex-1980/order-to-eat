291f596deca09955506c835873a505df6c2f4cb9
Chansoo <devhan1207@gmail.com>
2023-11-28 14:55:42 -0500

    started context for states

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import Carticon from "../Cart/CartIcon";
 import classes from "../Layout/HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
 
-const HeaderCartButton = ({onClick}) => {
+const HeaderCartButton = ({ onClick }) => {
+  const cartContext = useContext(CartContext);
+
+  const numberOfCartItems = cartContext.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
-    <button className={classes.button} onClick={onClick} >
+    <button className={classes.button} onClick={onClick}>
       <span className={classes.icon}>
         <Carticon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
